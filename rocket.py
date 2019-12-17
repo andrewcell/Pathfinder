af77aca3461fda7f41c6ac6aaa49340026901102
Andrew Bray <a@vxz.me>
2019-12-17 09:25:10 +0900

    Add CSRF token support

--- rocket.py
+++ rocket.py
@@ -1,24 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
-soup = BeautifulSoup(response.text, 'lxml')
-csrf_token = soup.select_one('meta[name="csrf-token"]')['content']
-
 class Rocket:
     def __init__(self, host, port):
         self.host = host
         self.port = port
-        self protocol = "http://"
+        self.protocol = "http://"
+        self.request = requests.Session()
     def returnHost(self):
         return self.host
     def returnPort(self):
         return self.port
     def HIT(self, path):
-        return requests.get(self.protocol + self.host + ":" + self.port + "/" + path)
+        return self.request.get(self.protocol + self.host + ":" + self.port + "/" + path)
+    def getCSRF(self):
+        page = self.HIT("admin/login")
+        soup = BeautifulSoup(page.text, "html.parser")
+        token = soup.find('input', {'name': '_csrf'})['value']
+        return token
     def POST(self, data, path):
-        page = self.HIT("/admin/login")
-        soup = BeautifulSoup(page, 'lxml')
-        csrf_token = soup.select_one('meta[name="csrf-token"]')['content']
-
-        return requests.post("http://"+self.host+":"+self.port+"/"+path, data=data)
+        token = self.getCSRF()
+        data["_csrf"] = token
+        return self.request.post(self.protocol+self.host+":"+self.port+"/"+path, data=data)
 
    
