90e7ebf6b4eda10ad3c5d1c02064033a4c4bd751
Andrew M. Bray <a@vxz.me>
2020-01-07 12:23:39 +0900

    Update for Pyinstaller file is now available

--- updater.py
+++ updater.py
@@ -2,7 +2,8 @@ import requests
 import json
 import difflib
 import sys
-
+import os
+import platform
 
 def checkUpdated(filename):
     print("--------------Validating " + filename + "--------------")
@@ -25,11 +26,56 @@ def updateFile(filename, newFile):
     file.close()
     print(filename + " is updated.")
 
+def pyinstallerUpdate():
+    path = os.path.dirname(sys.executable)
+    print("Pyinstaller prebuilt detected. Updating prebuilt file.")
+    print("Retrieving Latest release from Github...")
+    latest = json.loads(requests.get("https://api.github.com/repos/andrewcell/Pathfinder/releases").text)[0]
+    print("Downloading " + latest["tag_name"] + "...")
+    machine = platform.machine()
+    system = platform.system()
+    if machine.endswith("86"): # Check is 32bit only system.
+        print("Prebuilt for 32bit system is not available.")
+        sys.exit(1)
+
+    if system == "Darwin": # macOS only have amd64 system.
+        filename = "pathfinder-macos"
+    elif machine.startswith("arm", 0, 3) or machine == "aarch64": # Detect ARM
+        if system != "Linux": # If is not Linux
+            print("Prebuilt for ARM 64bit system only available in Linux.")
+            sys.exit(1)
+        else:
+            filename = "pathfinder-linux-aarch64"
+    elif system == "Linux":
+        filename = "pathfinder-linux-amd64"
+    elif system == "Windows":
+        if machine != "x86_64":
+            print("Prebuilt for amd64 is only available item on Windows.") # Maybe run on Windows on Arm
+        else:
+            filename = "pathfinder-windows.exe"
+
+    print("Downloading - " + filename)
+    for asset in latest["assets"]:
+        if asset["name"] == filename:
+            import shutil
+            data = requests.get(asset["browser_download_url"], stream=True)
+            with open(path + "/" + filename, "wb") as f:
+                data.raw.decode_content = True
+                shutil.copyfileobj(data.raw, f)
+            print("Download Completed - " + filename)
+            print("Update completed. " + filename + " available.")
+            sys.exit(0)
+    print("No option available. Run on proper environment.")
+    sys.exit(0)
 if __name__ == "__main__":
     print("Checking Newest Update")
     import pathfinder
     print("Current API Version : v" + pathfinder.v)
     del pathfinder
+    import nasa
+    nasa = nasa.Nasa(".")
+    if nasa.fileExists("pathfinder-linux-amd64") or nasa.fileExists("pathfinder-linux-aarch64") or nasa.fileExists("pathfinder-macos") or nasa.fileExists("pathfinder-windowsexe"):
+        pyinstallerUpdate()
     try:
         print("Trying Git Pull from origin...")
         import git
